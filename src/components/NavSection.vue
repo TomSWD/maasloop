e9729ef4c731449cdb686e69e7f19e9cb5a2b6b3
TomSWD <78202370+TomSWD@users.noreply.github.com>
2022-03-14 16:50:14 +0100

    Cleaning Code

--- src/components/NavSection.vue
+++ src/components/NavSection.vue
@@ -53,30 +53,13 @@
         <a class="nav__link" href="#">Kontakt</a>
         <a class="nav__link" href="#">FAQ</a>
       </div>
-      <!-- <label for="nav__lang-select">PL</label> -->
       <div class="nav__right-contener">
         <select class="nav__lang-select" name="language" id="language-select">
           <option class="nav__lang" value="pl">PL</option>
-
-          <!-- czy nieuzywane klasy zostawić czy usunąć??????? nav__lang-->
           <option class="nav__lang" value="en">EN</option>
           <option class="nav__lang" value="de">DE</option>
           <option class="nav__lang" value="gb">GB</option>
         </select>
-        <!-- <a href="#" class="nav__lang"
-        >PL<svg
-          width="9"
-          height="7"
-          viewBox="0 0 9 7"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M4.50071 6.06531C4.33941 6.06531 4.17813 6.00372 4.05516 5.88081L0.18536 2.01097C-0.06081 1.7648 -0.06081 1.36568 0.18536 1.11961C0.43143 0.873541 0.830472 0.873541 1.07666 1.11961L4.50071 4.54386L7.92477 1.11973C8.17094 0.873661 8.56995 0.873661 8.816 1.11973C9.06229 1.3658 9.06229 1.76492 8.816 2.01109L4.94626 5.88093C4.82322 6.00386 4.66195 6.06531 4.50071 6.06531Z"
-            fill="black"
-          />
-        </svg>
-       </a> -->
         <div class="hidden-btn">
           <a href="#" class="nav__btn-desktop btn">KUP</a>
         </div>
@@ -141,10 +124,6 @@ export default {
 </script>
 
 <style scoped lang="scss">
-// .nav {
-//   // max-width: 1440px;
-//   // margin: auto;
-// }
 .nav__container {
   display: flex;
   position: static;
@@ -185,10 +164,7 @@ export default {
 }
 .nav__right-contener {
   display: flex;
-  // justify-content: space-between;
   align-items: center;
-  // max-width: 120px;
-  // width: 100%;
 }
 .hidden-btn {
   display: none;
